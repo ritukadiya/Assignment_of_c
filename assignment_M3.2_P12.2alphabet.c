e2911133ec30f42f92897bdfe4a2fb92979ca1ca
ritukadiya <145090408+ritukadiya@users.noreply.github.com>
2023-10-11 12:04:14 +0530

    Update assignment_M3.2_P12.2alphabet.c

--- assignment_M3.2_P12.2alphabet.c
+++ assignment_M3.2_P12.2alphabet.c
@@ -5,27 +5,6 @@ D E F
 G H I J
 K L M N O
 */
-/*#include<stdio.h>
-main()
-{
-	
-	int rows,i,j; 
-	char c='A';
-	
-	printf("\n\t Enter a number of rows: ");
-	scanf("%d",&rows);
-  
-     
-    for (i = 0; i < rows; i++) 
-	{ 
-  		for (j = 0; j <= i; j++) 
-		{ 
-            printf("%c", c); 
-            c++;
-        } 
-        printf("\n"); 
-    } 
-}*/
 #include <stdio.h>
 main()
 {
