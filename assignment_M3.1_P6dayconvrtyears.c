d86b57719ef1bbeeb564e7bc7428eba648b9110b
ritukadiya <145090408+ritukadiya@users.noreply.github.com>
2023-11-01 10:58:28 +0530

    Update assignment_M3.1_P6dayconvrtyears.c

--- assignment_M3.1_P6dayconvrtyears.c
+++ assignment_M3.1_P6dayconvrtyears.c
@@ -9,16 +9,11 @@ main()
     printf("Enter number of days: ");
     scanf("%d", &number_of_days);
 
-    years = number_of_days / 365;  
-	
-
-    months = number_of_days - years *365 / 30;
+    years = number_of_days * 365;
     
-    days = number_of_days - years * 365 - months*30;
-	//days=(number_of_days%365)%7;
+    days =years/365;
     
     printf("Years = %d", years);
-    printf("\nMonths = %d", months);
     printf("\nDays = %d", days);
 
    
